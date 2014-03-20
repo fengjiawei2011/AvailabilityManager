1de1a22c9ec88a711b601d018ca6a53afd1a8fd8
Jiawei Feng <fengjiawei2011@gmail.com>
2014-03-20 00:23:50 -0700

    implemented host operation
    1. add host
    2. disconnect host
    3. remove host

--- src/main/java/com/cmpe283/project/AvailabilityManager.java
+++ src/main/java/com/cmpe283/project/AvailabilityManager.java
@@ -9,15 +9,21 @@ import java.util.logging.Logger;
 import org.tempuri.Service;
 import org.tempuri.ServiceSoap;
 
+import com.vmware.vim25.ClusterConfigSpec;
+import com.vmware.vim25.ComputeResourceConfigSpec;
 import com.vmware.vim25.FileFault;
+import com.vmware.vim25.HostConnectSpec;
 import com.vmware.vim25.InsufficientResourcesFault;
+import com.vmware.vim25.InvalidLogin;
 import com.vmware.vim25.InvalidProperty;
 import com.vmware.vim25.InvalidState;
 import com.vmware.vim25.RuntimeFault;
 import com.vmware.vim25.TaskInProgress;
+import com.vmware.vim25.TooManyHosts;
 import com.vmware.vim25.VirtualMachineMovePriority;
 import com.vmware.vim25.VirtualMachinePowerState;
 import com.vmware.vim25.VmConfigFault;
+import com.vmware.vim25.mo.ClusterComputeResource;
 import com.vmware.vim25.mo.ComputeResource;
 import com.vmware.vim25.mo.Datacenter;
 import com.vmware.vim25.mo.Folder;
@@ -33,40 +39,56 @@ public class AvailabilityManager {
 	private static final String USERNAME = "administrator";
 	private static final String PASSWORD = "12!@qwQW";
 
-	private static final Logger logger = Logger
-			.getLogger(AvailabilityManager.class.getName());
+	private static final Logger logger = Logger.getLogger(AvailabilityManager.class.getName());
 
 	public static void main(String[] args) throws MalformedURLException,
 			RemoteException, InterruptedException {
 		// TODO Auto-generated method stub
 		logger.log(Level.FINE, "logger test");
-		URL url = new URL(AvailabilityManager.URL);
-		ServiceInstance si = new ServiceInstance(url,
-				AvailabilityManager.USERNAME, AvailabilityManager.PASSWORD,
-				true);
-		
-		
 
-		//HostSystem[] hosts = AvailabilityManager.getHosts(si);
-		//AvailabilityManager.checkruning(si);
-		AvailabilityManager.run(si);
-		//ping(lanVM.getGuest().getIpAddress());
 		
 		
-//		HostSystem[] hosts = AvailabilityManager.getHosts(si);
-//		ComputeResource cr = (ComputeResource) hosts[1].getParent();
-//		hosts[0].getVms()[0].migrateVM_Task(cr.getResourcePool(), hosts[1], VirtualMachineMovePriority.lowPriority, VirtualMachinePowerState.poweredOff);
+		URL url = new URL(AvailabilityManager.URL);
+		ServiceInstance si = new ServiceInstance(url, AvailabilityManager.USERNAME, AvailabilityManager.PASSWORD, true);
+
+		
+		AvailabilityManager.hostOperation(si);
 		
 		si.getServerConnection().logout();
 
 	}
-	
-	public static void checkruning(ServiceInstance si) throws InvalidProperty, RuntimeFault, RemoteException, InterruptedException{
-		
+
+	public static void migrateAll(ServiceInstance si) throws InvalidProperty,
+			RuntimeFault, RemoteException, InterruptedException {
+		HostSystem[] hosts = AvailabilityManager.getHosts(si);
+		VirtualMachine[] vms = hosts[0].getVms();
+		for (VirtualMachine vm : vms) {
+			AvailabilityManager.coldMigrate(si, vm, hosts[1]);
+		}
+	}
+
+	public static void coldMigrate(ServiceInstance si, VirtualMachine vm, HostSystem hostDes)
+			throws InvalidProperty, RuntimeFault, RemoteException,
+			InterruptedException {
+		ComputeResource cr = (ComputeResource) hostDes.getParent();
+
+		if (vm.getRuntime().powerState == VirtualMachinePowerState.poweredOn) {
+			AvailabilityManager.powerOffVm(vm);
+		}
+		System.out.println("adding......");
+		Task migrateTask = vm.migrateVM_Task(cr.getResourcePool(), hostDes, VirtualMachineMovePriority.lowPriority, VirtualMachinePowerState.poweredOff);
+		if (migrateTask.waitForTask() == Task.SUCCESS) {
+			System.out.println("add successfully");
+		}
+	}
+
+	public static void checkruning(ServiceInstance si) throws InvalidProperty,
+			RuntimeFault, RemoteException, InterruptedException {
+
 		VirtualMachine[] vms = AvailabilityManager.getVMs(si);
-		while(true){
-			for(VirtualMachine vm: vms){
-				if(vm.getGuest().guestState.equals("notRunning"))
+		while (true) {
+			for (VirtualMachine vm : vms) {
+				if (vm.getGuest().guestState.equals("notRunning"))
 					System.out.println(vm.getName() + " " + vm.getGuest().guestState);
 			}
 			System.out.println();
@@ -74,15 +96,70 @@ public class AvailabilityManager {
 		}
 	}
 	
-	public static VirtualMachine getVM(ServiceInstance si, String hostName) throws InvalidProperty, RuntimeFault, RemoteException{
+	
+	
+	public static void hostOperation(ServiceInstance si) throws InvalidLogin,
+			RuntimeFault, RemoteException, InterruptedException {
+		HostConnectSpec newHost = new HostConnectSpec();
+		newHost.setHostName("130.65.132.159");
+		newHost.setUserName("root");
+		newHost.setPassword("12!@qwQW");
 		
+		newHost.setSslThumbprint("43:51:66:B8:3C:76:F5:8F:9A:63:90:0D:13:2C:25:B8:48:64:2D:6F");
+		
+
+		Datacenter dc = AvailabilityManager.getDatacenter(si); 
+		
+		// add host
+		Task addHostTask = dc.getHostFolder().addStandaloneHost_Task(newHost, new ComputeResourceConfigSpec(), true);
+		System.out.println("adding ........");
+		if (addHostTask.waitForTask() == Task.SUCCESS) {
+			//disconnect host
+			System.out.println(" add host successfully");
+			HostSystem host =(HostSystem)(new InventoryNavigator(si.getRootFolder()).searchManagedEntity("HostSystem", "130.65.132.159"));
+			Task disconTask = host.disconnectHost();
+			System.out.println("disconnecting ........");
+			if(disconTask.waitForTask() == Task.SUCCESS){
+				//remove host
+				System.out.println("disconnect success");
+				ComputeResource cr = (ComputeResource)host.getParent();
+				Task removehost = cr.destroy_Task(); // remove error  Host.Inventory.RemoveHostFromCluster
+				System.out.println("removing....");
+				if(removehost.waitForTask() == Task.SUCCESS){
+					System.out.println("remove succ");
+				}else{
+					System.out.println("remove error");
+				}
+			}else{
+				System.out.println("disconnect error");
+			}
+		} else {
+			// throw new HostConnectFault();
+			System.out.println("adding host erro!");
+		}
+	}
+
+	public static void moveHostToCluster(ServiceInstance si)
+			throws TooManyHosts, InvalidState, RuntimeFault, InvalidProperty,
+			RemoteException, InterruptedException {
+		ClusterComputeResource ccr = si.getRootFolder().createCluster("myCluster", new ClusterConfigSpec());
+		Task task = ccr.moveHostInto_Task(AvailabilityManager.getHosts(si)[0], ccr.getResourcePool());
+		if (task.waitForTask() == Task.SUCCESS) {
+			System.out.println("move success");
+		} else {
+			System.out.println("error");
+		}
+	}
+
+	public static VirtualMachine getVM(ServiceInstance si, String hostName)
+			throws InvalidProperty, RuntimeFault, RemoteException {
+
 		Folder f = si.getRootFolder();
 		return (VirtualMachine) new InventoryNavigator(f).searchManagedEntity("VirtualMachine", hostName);
 	}
-	
-	
-	
-	public static void run(ServiceInstance si) throws InvalidProperty, RuntimeFault, RemoteException, InterruptedException{
+
+	public static void run(ServiceInstance si) throws InvalidProperty,
+			RuntimeFault, RemoteException, InterruptedException {
 		IPContainer ipc = new IPContainer(si);
 		VirtualMachine[] vms = AvailabilityManager.getVMs(si);
 		int i = 0;
@@ -91,13 +168,12 @@ public class AvailabilityManager {
 			for (VirtualMachine vm : vms) {
 				// Printer.printVmName(vm);
 
-				if (vm.getName().equals("T03-VM01C-Win-Jiawei")
-						|| vm.getName().equals("T03-VM01C-Win-Yang")
-						//|| vm.getName().equals("T03-VM02-Lin-Ling")
-						//|| vm.getName().equals("T03-VM03-Lin-Ruiyun")
-						//|| vm.getName().equals("T03-VM02-Lin-Lan")
-						) { 
-					    											
+				if (vm.getName().equals("T03-VM01C-Win-Jiawei") || vm.getName().equals("T03-VM01C-Win-Yang")
+				// || vm.getName().equals("T03-VM02-Lin-Ling")
+				// || vm.getName().equals("T03-VM03-Lin-Ruiyun")
+				// || vm.getName().equals("T03-VM02-Lin-Lan")
+				) {
+
 					continue;
 				}
 				setSelectedVM(vm);
@@ -107,34 +183,27 @@ public class AvailabilityManager {
 				}
 
 				while (vm.getGuest().guestState.equals("notRunning")) {
-					System.out.println(vm.getName() + "  "
-							+ vm.getGuest().guestState);
+					System.out.println(vm.getName() + "  " + vm.getGuest().guestState);
 					Thread.sleep(2000);
 				}
-				System.out.println("---- OS is " + vm.getGuest().guestState
-						+ "-----");
+				System.out.println("---- OS is " + vm.getGuest().guestState + "-----");
 				if (AvailabilityManager.hasIpAddress(vm)) {
 					String ip = ipc.getIP();
 					vm.getGuest().setIpAddress(ip);
 					System.out.println("set ip " + ip + " for " + vm.getName());
-					System.out.println("Current ip of "+vm.getName()+" is " + vm.getGuest().getIpAddress() );
-					
-				}else{
+					System.out.println("Current ip of " + vm.getName() + " is " + vm.getGuest().getIpAddress());
+
+				} else {
 					System.out.println(vm.getName() + " has ip address already : " + vm.getGuest().getIpAddress());
 				}
 
 				while (!MyPing.pingHost(vm.getGuest().getIpAddress())) {
-					System.out.println("Ping " + vm.getGuest().getIpAddress()
-							+ " Result : "
-							+ MyPing.pingHost(vm.getGuest().getIpAddress()));
+					System.out.println("Ping " + vm.getGuest().getIpAddress() + " Result : " + MyPing.pingHost(vm.getGuest().getIpAddress()));
 					Thread.sleep(2000);
 				}
 
-				
-				System.out.println("Ping " + vm.getGuest().getIpAddress()
-						+ " Result : "
-						+ MyPing.pingHost(vm.getGuest().getIpAddress()));
-				
+				System.out.println("Ping " + vm.getGuest().getIpAddress() + " Result : " + MyPing.pingHost(vm.getGuest().getIpAddress()));
+
 				System.out.println(vm.getName() + " is powering off!");
 				System.out.println();
 				vm.powerOffVM_Task();
@@ -145,19 +214,13 @@ public class AvailabilityManager {
 
 	public static void setSelectedVM(VirtualMachine vm) {
 		System.out.println("Name : " + vm.getName());
-		System.out.println("Guest OS:"
-				+ vm.getSummary().getConfig().guestFullName);
+		System.out.println("Guest OS:" + vm.getSummary().getConfig().guestFullName);
 		System.out.println("VM Version:" + vm.getConfig().version);
-		System.out.println("CPU:" + vm.getConfig().getHardware().numCPU
-				+ " vCPU");
-		System.out.println("Memory:" + vm.getConfig().getHardware().memoryMB
-				+ " MB");
-		System.out.println("Memory Overhead:"
-				+ (long) vm.getConfig().memoryAllocation.reservation / 1000000f
-				+ " MB");
+		System.out.println("CPU:" + vm.getConfig().getHardware().numCPU + " vCPU");
+		System.out.println("Memory:" + vm.getConfig().getHardware().memoryMB + " MB");
+		System.out.println("Memory Overhead:" + (long) vm.getConfig().memoryAllocation.reservation / 1000000f + " MB");
 		System.out.println("VMware Tools:" + vm.getGuest().toolsRunningStatus);
-		System.out.println("IP Addresses:"
-				+ vm.getSummary().getGuest().getIpAddress());
+		System.out.println("IP Addresses:" + vm.getSummary().getGuest().getIpAddress());
 		System.out.println("State:" + vm.getGuest().guestState);
 		System.out.println();
 	}
@@ -167,11 +230,9 @@ public class AvailabilityManager {
 			InsufficientResourcesFault, RuntimeFault, RemoteException,
 			InterruptedException {
 		Task task = vm.powerOnVM_Task(null);
-		System.out.println("VirtualMachine " + vm.getName()
-				+ " now is powerring on! Please wait...");
+		System.out.println("VirtualMachine " + vm.getName() + " now is powerring on! Please wait...");
 		if (task.waitForTask() == Task.SUCCESS) {
-			System.out.println("VirtualMachine " + vm.getName()
-					+ " is powered on successfully!");
+			System.out.println("VirtualMachine " + vm.getName() + " is powered on successfully!");
 		}
 	}
 
@@ -184,31 +245,26 @@ public class AvailabilityManager {
 			InsufficientResourcesFault, RuntimeFault, RemoteException,
 			InterruptedException {
 		Task task = vm.powerOffVM_Task();
-		System.out.println("VirtualMachine " + vm.getName()
-				+ " now is powerring off! Please wait...");
+		System.out.println("VirtualMachine " + vm.getName() + " now is powerring off! Please wait...");
 		if (task.waitForTask() == Task.SUCCESS) {
-			System.out.println("VirtualMachine " + vm.getName()
-					+ " is powered off successfully!");
+			System.out.println("VirtualMachine " + vm.getName() + " is powered off successfully!");
 		}
 	}
 
 	public static boolean isVmPoweredOn(VirtualMachine vm) {
 		VirtualMachinePowerState vmps = vm.getRuntime().getPowerState();
 		if (vmps == VirtualMachinePowerState.poweredOn) {
-			System.out.println("VirtualMachine " + vm.getName()
-					+ " has been powered on!");
+			System.out.println("VirtualMachine " + vm.getName() + " has been powered on!");
 			return true;
 		}
-		System.out.println("VirtualMachine " + vm.getName()
-				+ " now is NOT powered on! Please Power on it!");
+		System.out.println("VirtualMachine " + vm.getName() + " now is NOT powered on! Please Power on it!");
 		return false;
 	}
 
 	public static boolean isVmPoweredOff(VirtualMachine vm) {
 		VirtualMachinePowerState vmps = vm.getRuntime().getPowerState();
 		if (vmps == VirtualMachinePowerState.poweredOff) {
-			System.out.println("VirtualMachine " + vm.getName()
-					+ " has been powered off!");
+			System.out.println("VirtualMachine " + vm.getName() + " has been powered off!");
 			return true;
 		}
 		// System.out.println("VirtualMachine "+vm.getName()+" is powered on!");
@@ -236,8 +292,7 @@ public class AvailabilityManager {
 			throws InvalidProperty, RuntimeFault, RemoteException {
 		// HostSystem host = null;
 		Folder rootFolder = si.getRootFolder();
-		ManagedEntity[] hosts = new InventoryNavigator(rootFolder)
-				.searchManagedEntities("HostSystem");// "130.65.132.159"
+		ManagedEntity[] hosts = new InventoryNavigator(rootFolder).searchManagedEntities("HostSystem");// "130.65.132.159"
 
 		if (hosts == null) {
 			System.out.println("Cannot find the host");
